584c78b38d4e2554d3e294bebb68eea760e65754
praveenn76 <praveentatimatla@gmail.com>
2023-08-21 12:32:25 +0530

    new2

---
+++ hcl/com/sample.java
@@ -0,0 +1,30 @@
+package hcl.com;
+
+public class sample {
+	
+
+	
+	public static void main(String[] args) {
+	
+	
+	String str= "greens";
+	
+		StringBuffer strbuffer=new StringBuffer(str);
+		 System.out.println(strbuffer);
+		 
+		 strbuffer.reverse();
+		 System.out.println(strbuffer);
+		 
+	
+	
+	
+	
+
+		
+	}
+
+		
+		
+	}
+
+
